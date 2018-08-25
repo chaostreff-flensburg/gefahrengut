a08e91a30c0c25c36756f81c531df607a87ecaf7
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-08-25 21:22:57 +0200

    Added random high power mode

--- gefahrengut/gefahrengut.ino
+++ gefahrengut/gefahrengut.ino
@@ -2,10 +2,14 @@ void setup() {
   pinMode(D5, OUTPUT);
 }
 
-
 void loop() {
+  int powerMode = ceil(random(0,101));
   digitalWrite(D5, HIGH);
   delay(random(500, 2000));
+  if (powerMode == 100) {
+    digitalWrite(D5, HIGH);
+    delay(10000);
+  };
   digitalWrite(D5, LOW);
   delay(random(5000, 10000));
 }
